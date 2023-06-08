a8f3eb92f938c14e25eed6eddfec485eb10f2f23
Benjamin Levesque <14175665+benjlevesque@users.noreply.github.com>
2023-06-08 17:44:34 +0200

    refactor: use go-gh

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/benjlevesque/gh-bump
 go 1.16
 
 require (
+	github.com/cli/go-gh v1.2.1
 	github.com/coreos/go-semver v0.3.0
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
--- pkg/gh/gh.go
+++ pkg/gh/gh.go
@@ -1,11 +1,10 @@
 package gh
 
 import (
-	"bytes"
 	"errors"
-	"fmt"
-	"os/exec"
 	"strings"
+
+	"github.com/cli/go-gh"
 )
 
 func GetLatestRelease() (string, error) {
@@ -13,53 +12,20 @@ func GetLatestRelease() (string, error) {
 }
 
 func CreateRelease(tag string) (string, error) {
-	str, _, err := gh_exec("release", "create", "--generate-notes", tag)
+	str, _, err := gh.Exec("release", "create", "--generate-notes", tag)
 	return str.String(), err
 
 }
 
-func resolveRepository() (string, error) {
-	viewOut, err := gh_exec_clean("repo", "view")
-	if err != nil {
-		return "", err
-	}
-	repo := strings.TrimSpace(strings.Split(viewOut, ":")[1])
-	return repo, nil
-
-}
-
 func gh_exec_clean(args ...string) (string, error) {
-	sout, eout, err := gh_exec(args...)
+	sout, eout, err := gh.Exec(args...)
 	if err != nil {
 		if strings.Contains(eout.String(), "not a git repository") {
-			return "", errors.New("Try running this command from inside a git repository or with the -R flag")
+			return "", errors.New("try running this command from inside a git repository or with the -R flag")
 		}
 		return "", err
 	}
 	viewOut := strings.Split(sout.String(), "\n")[0]
 
 	return viewOut, nil
-}
-
-// From https://github.com/vilmibm/gh-contribute
-
-// gh shells out to gh, returning STDOUT/STDERR and any error
-func gh_exec(args ...string) (sout, eout bytes.Buffer, err error) {
-	ghBin, err := exec.LookPath("gh")
-	if err != nil {
-		err = fmt.Errorf("could not find gh. Is it installed? error: %w", err)
-		return
-	}
-
-	cmd := exec.Command(ghBin, args...)
-	cmd.Stderr = &eout
-	cmd.Stdout = &sout
-
-	err = cmd.Run()
-	if err != nil {
-		err = fmt.Errorf("failed to run gh. error: %w, stderr: %s", err, eout.String())
-		return
-	}
-
-	return
 }
